d2aafce7b1b245e6fb93ba368bb3f084b7c15feb
ilyas7737 <131005106+ilyas7737@users.noreply.github.com>
2023-05-18 16:18:42 +0300

    git comit -m"solution_2"

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,24 +34,27 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {client: {name, phone}, order: {adress, sum},  goods: []}};
-
-
-    data.order.address = "ул. " + ${address.street} + ", 
-        дом " + ${address.house} + ", " + ${address.entrance} + " 
-        подъезд, " + ${address.floor} + " 
-        этаж, кв. " + ${address.flat};
-
-    data.order.sum = sum;
+    let data = {client: {name, phone}, order: {address, sum}, goods: [{title, count}]};
 
     let countOfGoods = goods.length;
-    for (let i = 0; i <= countOfGoods; i += 1) {
+
+    for (let i = 0; i < countOfGoods; i += 1) {
         data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
+
+    data.order.address = 
+    " ул. " + ${address.street} + 
+    " дом " + ${address.house} + 
+    ", " + ${address.entrance} + 
+    " подъезд, " + ${address.floor} + 
+    " этаж, кв. " + ${address.flat};
+
+    data.order.sum = sum;
+
     data.client = ${client.name} + " " + ${client.phone};
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data});
 
     return jsonData;
 }
